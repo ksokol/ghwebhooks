d5f5fcb19394528e3a847e4209959816a40acb99
Kamill Sokol <dev@sokol-web.de>
2019-02-01 19:50:33 +0100

    Only allow one deployment of a specific app at the same time.

--- main.go
+++ main.go
@@ -6,15 +6,27 @@ import (
 	"ghwebhooks/security"
 	"log"
 	"net/http"
+	"sync"
 )
 
 func main() {
 	config := config.LoadConfig()
 
+	var activeDeployments sync.Map
+
 	http.HandleFunc("/", security.Secured(func(resp http.ResponseWriter, req *http.Request) {
 		for _, app := range config.Apps {
 			if app.Name == req.URL.Path[1:] {
+				_, loaded := activeDeployments.LoadOrStore(app.Name, nil)
+
+				if loaded == true {
+					resp.WriteHeader(409)
+					return
+				}
+
 				deploy.Deploy(app.Dir, &config)
+				activeDeployments.Delete(app.Name)
+
 				return
 			}
 		}
