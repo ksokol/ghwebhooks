5db93ec995c681cec1c9b9557e7c6514236542ad
Kamill Sokol <dev@sokol-web.de>
2019-02-24 17:32:15 +0100

    Pass artefact url to python script.

--- deploy/deploy.go
+++ deploy/deploy.go
@@ -11,7 +11,7 @@ import (
 
 func Deploy(context *context.Context, status *types.Status) {
 	os.Chdir(context.AppDir)
-	out, err := exec.Command("python", "cron.py").Output()
+	out, err := exec.Command("python", "cron.py", context.ArtefactURL).Output()
 
 	if err != nil {
 		status.Fail(err)
