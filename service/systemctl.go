ddc6527aaa133006ff214919454fb810de8e2a20
Kamill Sokol <dev@sokol-web.de>
2021-04-03 06:01:44 +0200

    fix: ignore current state. restart service

--- service/systemctl.go
+++ service/systemctl.go
@@ -2,39 +2,21 @@ package service
 
 import (
 	"os/exec"
-	"strings"
 )
 
-const (
-	active   = "active"
-	inactive = "inactive"
-	unknown  = "unknown"
-)
-
-func Start(service string) (bool, error) { return command("start", inactive, service) }
-
-func Stop(service string) (bool, error) { return command("stop", active, service) }
-
-func command(command string, expectedState string, service string) (bool, error) {
-	if currentState, err := getServiceState(service); err != nil || currentState != expectedState {
-		return false, err
-	}
-	return true, systemctl(command, service)
+func Start(service string) (bool, error) {
+	return command("restart", service)
 }
 
-func getServiceState(service string) (string, error) {
-	out, err := exec.Command("systemctl", "is-active", service).Output()
-	state := strings.TrimSuffix(string(out[:]), "\n")
+func Stop(service string) (bool, error) {
+	return command("stop", service)
+}
 
-	/*
-	 systemctl returns code 3 in case of an inactive or unknown service.
-	 Therefore we need to check stdout for the unit state before.
-	*/
-	if state == active || state == inactive || state == unknown {
-		return state, nil
+func command(command string,  service string) (bool, error) {
+	if err := systemctl(command, service); err != nil {
+		return false, err
 	}
-
-	return "", err
+	return true, nil
 }
 
 func systemctl(arg ...string) error {
